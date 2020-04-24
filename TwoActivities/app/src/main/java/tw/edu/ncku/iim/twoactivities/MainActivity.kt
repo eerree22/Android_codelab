d3597f1133059ca769d3fbbd234183fd8cb5746b
張皓偉 <a26421965@hotmail.com.tw>
2020-04-24 17:54:05 +0800

    0421課堂練習

--- TwoActivities/app/src/main/java/tw/edu/ncku/iim/twoactivities/MainActivity.kt
+++ TwoActivities/app/src/main/java/tw/edu/ncku/iim/twoactivities/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
@@ -13,9 +14,59 @@ class MainActivity : AppCompatActivity() {
         const val myRequestCode = 666
     }
 
+    private val LOG_TAG = MainActivity::class.java.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(LOG_TAG,"正在呼叫onCreate")
+
+        if (savedInstanceState!=null)
+        {
+            val myText=savedInstanceState.getString("myText")
+
+            val myTextView=findViewById<TextView>(R.id.text_message_main)
+            myTextView.text=myText
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.d(LOG_TAG,"正在呼叫onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(LOG_TAG,"正在呼叫onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(LOG_TAG,"正在呼叫onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(LOG_TAG,"正在呼叫onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(LOG_TAG,"正在呼叫onDestroy")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        //刷新前先將文字儲存起來
+        val myText=findViewById<TextView>(R.id.text_message_main).text.toString()
+
+        if (myText.isNotEmpty())
+        {
+            outState.putString("myText",myText)
+        }
     }
 
     fun launchSecondActivity(view: View) {
--- TwoActivities/app/src/main/java/tw/edu/ncku/iim/twoactivities/SecondActivity.kt
+++ TwoActivities/app/src/main/java/tw/edu/ncku/iim/twoactivities/SecondActivity.kt
@@ -4,11 +4,14 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
+
+    private val LOG_TAG = MainActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +21,34 @@ class SecondActivity : AppCompatActivity() {
         val myTextView=findViewById<TextView>(R.id.text_message)
 
         myTextView.text=message
+
+        Log.d(LOG_TAG,"Second_onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.d(LOG_TAG,"Second_onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(LOG_TAG,"Second_onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(LOG_TAG,"Second_onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(LOG_TAG,"Second_onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(LOG_TAG,"Second_onDestroy")
     }
 
     fun returnReply(view: View) {
@@ -28,6 +59,7 @@ class SecondActivity : AppCompatActivity() {
         val intent= Intent()
         intent.putExtra("message",myMessage)
         setResult(Activity.RESULT_OK,intent)
+        Log.d(LOG_TAG,"End secondActivity")
         finish()
     }
 }
